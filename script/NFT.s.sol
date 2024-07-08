f5638528a0bfc70408e2f18c5854220f1521ec23
chongkw <chongkw@phillip.com.sg>
2024-07-08 09:45:25 +0800

    fix: update nft script

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -5,11 +5,15 @@ import "forge-std/Script.sol";
 import "../src/NFT.sol";
 
 contract MyNFTScript is Script {
-    function run() external {
+    function run(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI
+    ) external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-       
-        new NFT("Papers", "PR", "google.com");
+
+        new NFT(_name, _symbol, _baseURI);
 
         vm.stopBroadcast();
     }
